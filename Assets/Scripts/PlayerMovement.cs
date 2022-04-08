eaf8765ec61a028a52419cb75fa5d62c2e2af4ac
jmyersle <50622475+jmyersle@users.noreply.github.com>
2022-04-08 14:20:16 -0300

    .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,11 +17,15 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask ground;
     bool isOnGround;
 
+    private bool IsMoving;
+    public AudioSource step1;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        step1 = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -37,10 +41,35 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+        move = Vector3.ClampMagnitude(move, 1f);
 
         character.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetKeyDown("space") && isOnGround)
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            IsMoving = true;
+        }
+        else
+        {
+            IsMoving = false;
+        }
+
+        if (IsMoving && !step1.isPlaying)
+        {
+            step1.Play();
+        }
+        if (!IsMoving)
+        {
+            step1.Stop();
+        }
+        if (!isOnGround)
+        {
+            step1.Stop();
+        }
+
+
+        if (Input.GetKeyDown("space") && isOnGround)
         {
             velocity.y = Mathf.Sqrt(jump * -2f * gravity);
         }
