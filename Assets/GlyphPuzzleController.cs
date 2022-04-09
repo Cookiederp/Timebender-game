27a61239b3d37a55db051cf81e6e38cb1c48e967
Alec <91696594+ABelliv-UNB@users.noreply.github.com>
2022-04-09 17:42:37 -0300

    Made Some Glyphs

---
+++ Assets/GlyphPuzzleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlyphPuzzleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject SkeletonPrefab;
+    public GameObject PositiveXGlyph;
+    public GameObject NegativeXGlyph;
+    public GameObject PositiveZGlyph;
+    public GameObject NegativeZGlyph;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
