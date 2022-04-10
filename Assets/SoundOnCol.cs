918cd8b0fad5dbc2b5712a620da2c9d9bf95bc99
Cookiederp <73700220+Cookiederp@users.noreply.github.com>
2022-04-10 01:32:01 -0300

    more sounds

---
+++ Assets/SoundOnCol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnCol : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(rb.velocity.sqrMagnitude > 5)
+        audioSource.Play();
+    }
+}
