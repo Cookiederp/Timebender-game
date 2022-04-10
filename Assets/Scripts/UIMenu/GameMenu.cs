f5f832bffa5eba832b14bcb80de96ba561fe53df
Cookiederp <73700220+Cookiederp@users.noreply.github.com>
2022-04-10 12:18:32 -0300

    reset

--- Assets/Scripts/UIMenu/GameMenu.cs
+++ Assets/Scripts/UIMenu/GameMenu.cs
@@ -53,4 +53,9 @@ public class GameMenu : MonoBehaviour
         Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
+
+    public void OnReset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
